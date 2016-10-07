0075511b39df47c87d6b347fa5be6772ca1d4a80
wloczykij25 <wloczykij25@gmail.com>
2016-10-07 13:29:52 +0200

    Przykład Castle Windsor

---
+++ WindsorCastleTest/Class1.cs
@@ -0,0 +1,41 @@
+﻿
+namespace WindsorCastleTest
+{
+    public class Main
+    {
+        private IDependency1 object1;
+        private IDependency2 object2;
+
+        public Main(IDependency1 dependency1, IDependency2 dependency2)
+        {
+            object1 = dependency1;
+            object2 = dependency2;
+        }
+
+        public void DoSomething()
+        {
+            object1.SomeObject = "Hello World";
+            object2.SomeOtherObject = "Hello Mars";
+        }
+    }
+
+    public interface IDependency1
+    {
+        object SomeObject { get; set; }
+    }
+
+    public interface IDependency2
+    {
+        object SomeOtherObject { get; set; }
+    }
+
+    public class Dependency1 : IDependency1
+    {
+        public object SomeObject { get; set; }
+    }
+
+    public class Dependency2 : IDependency2
+    {
+        public object SomeOtherObject { get; set; }
+    }
+}
---
+++ WindsorCastleTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Castle.Windsor;
+
+namespace WindsorCastleTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // CREATE A WINDSOR CONTAINER OBJECT AND REGISTER THE INTERFACES, AND THEIR CONCRETE IMPLEMENTATIONS.
+            var container = new WindsorContainer();
+            container.AddComponent("Main", typeof(Main));
+            container.AddComponent("Dependency1", typeof(IDependency1), typeof(Dependency1));
+            container.AddComponent("Dependency2", typeof(IDependency2), typeof(Dependency2));
+
+            // CREATE THE MAIN OBJECT AND INVOKE ITS METHOD(S) AS DESIRED.
+            Main MainThing = container.Resolve<Main>();
+            MainThing.DoSomething();
+        }
+    }
+}
